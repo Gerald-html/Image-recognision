939516c3d71cf506d931a0bc4ff258d69a3c9735
Gerald-html <geze7995@gmail.com>
2026-09-10 12:48:38 +0100

    Initial commit

---
+++ machine_learning.py
@@ -0,0 +1,33 @@
+from imageai.Classification import ImageClassification
+import os
+import torch
+
+# Fix PyTorch weights loading issue
+_original_load = torch.load
+torch.load = lambda f, *args, **kwargs: _original_load(f, *args, weights_only=False, **kwargs)
+
+execution = os.getcwd()
+prediction = ImageClassification()
+prediction.setModelTypeAsResNet50()
+prediction.setModelPath(os.path.join(execution, "resnet50-19c8e357.pth"))
+prediction.loadModel()
+
+predictions, probabilities = prediction.classifyImage(os.path.join(execution, "Godzilla.jpg"), result_count=15)
+for eachPrediction, eachProbability in zip(predictions, probabilities):
+    print(eachPrediction , " : " , eachProbability)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
